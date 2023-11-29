5f0442243cc7296671785dff35919dce8d231a6d
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-11-30 01:09:20 +0600

    added basic setup with interface and schema

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,81 @@
+import { Schema, model } from 'mongoose';
+import { IUser } from './users.interface';
+
+// const userSchema = new Schema<IUser>({
+//     name: { type: String, required: true },
+//     email: { type: String, required: true },
+//     avatar: String
+
+const userSchema = new Schema<IUser>({
+  userId: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+    default: [],
+  },
+  address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  orders: [
+    {
+      productName: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+export const User = model<IUser>('User', userSchema);
